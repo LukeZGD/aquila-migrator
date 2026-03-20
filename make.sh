28630e73ad91aeb9bb95dfed26d304d68d9c8c6c
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2026-03-20 23:17:13 +0800

    an update

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+rm *.deb
+sudo chown -R root: com.lukezgd.aquila-migrator com.lukezgd.aquila-unmigrator
 dpkg-deb -Zgzip -b com.lukezgd.aquila-migrator
 dpkg-deb -Zgzip -b com.lukezgd.aquila-unmigrator
+sudo chown -R $USER: com.lukezgd.aquila-migrator com.lukezgd.aquila-unmigrator
